c7669f357a7455ed0cca6c65ff519362e02ed6ce
SaburovPavel <137547144+SaburovPavel@users.noreply.github.com>
2023-08-20 14:57:32 +0300

    Add files via upload

---
+++ PassChip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceApp
+{
+    public class PassChip
+    {
+        public int Id { get; set; }
+        public int? UserId { get; set; }
+        public int CompId { get; set; }
+        public bool Duplicate { get; set; }
+        public bool Confirm { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual Comp Comp { get; set; }
+    }
+}
